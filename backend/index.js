48c76bc2c1762799740dc6a16d90a4fdcf5db1ea
zibaakord <zibaaakord@gmail.com>
2026-01-26 12:14:56 +0330

    basics

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { Pool } = require('pg');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'attendance_system',
+  password: '52315078',
+  port: 5432,
+});
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
